86e0bd6c5a6d5f40f0595c622c08e13d7d2450fa
nozwock <57829219+nozwock@users.noreply.github.com>
2026-03-24 13:59:06 +0530

    Add AlwaysWarp mod

---
+++ AlwaysWarp/Plugin.cs
@@ -0,0 +1,65 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AlwaysWarp;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+    static Harmony harmony;
+
+    public override void Load()
+    {
+        Log = base.Log;
+
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        try
+        {
+            harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), MyPluginInfo.PLUGIN_GUID);
+            foreach (var method in harmony.GetPatchedMethods())
+            {
+                Log.LogInfo($"Patched method: {method.DeclaringType.FullName}.{method.Name}");
+            }
+            if (!harmony.GetPatchedMethods().Any())
+            {
+                Log.LogError($"No tHarmony patches were applied.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.LogError($"Harmony patching failed: {ex}");
+        }
+    }
+
+    public override bool Unload()
+    {
+        Log.LogInfo($"Unloading plugin {MyPluginInfo.PLUGIN_GUID}");
+        harmony?.UnpatchSelf();
+        return true;
+    }
+
+    [HarmonyPatch]
+    class Patches
+    {
+        // GUI_CheckpointOptionsMenu
+        //      .panelsData
+        //          GUI_MENU_WarpMenu [2]
+        //
+        // Called in MapHandler_Core.CreateWarpMarkersRoutine() (Enumerator function)
+        [HarmonyPrefix]
+        [HarmonyPatch(
+            typeof(MapHandler_InteractableMarker_Checkpoint),
+            nameof(MapHandler_InteractableMarker_Checkpoint.SetCheckpoint))]
+        static void MapHandler_InteractableMarker_Checkpoint_SetCheckpoint_Prefix(ref bool isActive)
+        {
+            isActive = true;
+        }
+    }
+}
---
+++ AlwaysWarp/README.md
@@ -0,0 +1,5 @@
+# Always On Warp
+
+Makes the Warp ability available from the very start of the game, which lets the player teleport between save points.
+
+Last tested against the demo build `v0.9.17048D`.
